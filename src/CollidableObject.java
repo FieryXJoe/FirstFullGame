b9a313403df5ff282176eb3358493f92f59347af
FieryXJoe <josephmsherry@comcast.net>
2015-02-12 09:18:38 -0500

    Added Spikes
    Level gen code for spikes

--- src/CollidableObject.java
+++ src/CollidableObject.java
@@ -163,6 +163,15 @@ public abstract class CollidableObject
 				}
 				else
 					return new MovingPlatform(x, y, glGenVertexArrays(), glGenBuffers(), glGenBuffers(), glGenBuffers());
+			case 4: 
+				if(s.contains("[") && s.contains("]"))
+				{
+					float xOffset = Float.parseFloat((s.substring(s.indexOf("[") + 1, s.indexOf(";", s.indexOf("[")))));
+					float yOffset = Float.parseFloat(s.substring(s.indexOf(";", s.indexOf("[")) + 1,s.indexOf("]")));
+					return new Spikes(x + xOffset, y + yOffset, glGenVertexArrays(), glGenBuffers(), glGenBuffers(), glGenBuffers());
+				}
+				else
+					return new Spikes(x, y, glGenVertexArrays(), glGenBuffers(), glGenBuffers(), glGenBuffers());
 		}
 		return null;
 	}
