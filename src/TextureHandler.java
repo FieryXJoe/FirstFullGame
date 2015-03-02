78979f495c255e1e13252104feaa0f23383574d2
FieryXJoe <josephmsherry@comcast.net>
2015-03-02 11:02:42 -0500

    Ability to get byte array from an image
    unsure of order of RGB values in the int but I am guessing it is RGBA it
    could be ARGB or some other wierd order

--- src/TextureHandler.java
+++ src/TextureHandler.java
@@ -7,16 +7,29 @@ import javax.imageio.ImageIO;
 
 public class TextureHandler
 {
-	public boolean loadTexture2d(String path)
+	public byte[] loadTexture2d(String path)
 	{
 		 BufferedImage img;
 		 try{
 			 img = ImageIO.read(new File(path));
 		 }catch(IOException e){
 			 System.out.println("Texture at path: " + path + " is missing or damaged.");
-			 return false;
+			 return null;
 		 }
+		 byte[] pixelArray = convertPngToByteArray(img);
 		 
-		 return true;
+		 return pixelArray;
+	}
+	public byte[] convertPngToByteArray(BufferedImage img)
+	{
+		byte[] b = new byte[img.getHeight() * img.getHeight() * 4];
+		for(int i = 0; i < b.length; i += 4)
+		{
+				b[i] = (byte)((img.getRGB((i/4)%img.getWidth(), (i/4)/img.getWidth()) >> 24) % 255);
+				b[i + 1] = (byte)((img.getRGB((i/4)%img.getWidth(), (i/4)/img.getWidth()) >> 16) % 255);
+				b[i + 2] = (byte)((img.getRGB((i/4)%img.getWidth(), (i/4)/img.getWidth()) >> 8) % 255);
+				b[i + 3] = (byte)(img.getRGB((i/4)%img.getWidth(), (i/4)/img.getWidth()) % 255);
+		}
+		return b;
 	}
 }
